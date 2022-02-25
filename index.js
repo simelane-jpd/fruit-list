bad4ff211d851503dc41733201d623a146ad85fe
simelane-jpd <201575227@student.uj.ac.za>
2022-02-25 13:47:59 +0200

    added fruit names

---
+++ index.js
@@ -0,0 +1,3 @@
+const fruitNames = ["Grapes", "Melon", "Watermelon", "Tangerine", "Lemon", "Banana", "Pineapple", "Mango", "Red Apple"];
+
+const fruits = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎"];
