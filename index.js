4ef767c79fe8628a6f9af298ceefd0b8e14b86fb
simelane-jpd <201575227@student.uj.ac.za>
2022-02-28 17:21:44 +0200

    created a list, added new style, created a search bar

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const fruitNames = ["Grapes", "Melon", "Watermelon", "Tangerine", "Lemon", "Banana", "Pineapple", "Mango", "Red Apple"];
+const fruitNames = ["Banana 🍌","Grapes 🍇","Lemon 🍋","Mango 🥭", "Melon 🍈", "Pineapple 🍍","Red Apple 🍎","Tangerine 🍊","Watermelon 🍉"];
 
-const fruits = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎"];
+const emojis = ["🍌","🍇","🍋", "🥭","🍈", "🍍","🍎","🍊","🍉"];
+
+
+    function myFruits() {
+        
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('list');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("fruits");
+        li = ul.getElementsByTagName('li');
+      
+       
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+      }
+      document.getElementById("container").appendChild(select);
+    
